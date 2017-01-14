8e1347cfd8f1743a72260d418096a22e2a75897c
Rolf Paetow <rpa@dndapps.com>
2017-01-14 15:27:16 +0100

    add svg as image

--- src/js/main.js
+++ src/js/main.js
@@ -19,25 +19,14 @@ function init() {
 
 }
 
-var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
-    '<foreignObject width="100%" height="100%">' +
-    '<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:40px">' +
-    '<em>I</em> like ' +
-    '<span style="color:white; text-shadow:0 0 2px blue;">' +
-    'cheese</span>' +
-    '</div>' +
-    '</foreignObject>' +
-    '</svg>';
-
-var DOMURL = window.URL || window.webkitURL || window;
 var test = new Image();
-var svg = new Blob([data], {type: 'image/svg+xml'});
-var url = DOMURL.createObjectURL(svg);
-test.src = url;
+test.src = "/medienimweb/src/svg/car-svgrepo-com.svg";
+test.width = 75;
+test.height = 75;
 test.drawTest = function () {
-    context.drawImage(test, 0, 0);
-    DOMURL.revokeObjectURL(url);
+    context.drawImage(test, 0, 0, this.height, this.width);
 }
+
 
 
 var bird = {
